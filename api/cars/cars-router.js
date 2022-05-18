984980994e6d868974b2337fdf33aaab92997897
Nathan Cai <wenjuncai97@gmail.com>
2022-05-18 02:58:32 -0400

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+const Car = require('./cars-model')
+const {
+    checkCarId
+} = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('posting car')
+})
+
+module.exports = router;
